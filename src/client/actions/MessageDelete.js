629752456f973247a8df7fa5ae34563737810c78
KEON108 <86961848+KEON108@users.noreply.github.com>
2022-07-06 18:13:36 +0530

    Update MessageDelete.js

--- src/client/actions/MessageDelete.js
+++ src/client/actions/MessageDelete.js
@@ -10,10 +10,9 @@ class MessageDeleteAction extends Action {
   handle(data) {
     const client = this.client;
     const channel = client.channels.get(data.channel_id);
-    let message;
 
     if (channel) {
-      message = this.channel.messages.get(data.id);
+      let message = channel.messages.get(data.id);
       if (message) {
         channel.messages.delete(message.id);
         this.deleted.set(channel.id + message.id, message);
